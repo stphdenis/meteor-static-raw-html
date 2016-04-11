99779cb67b772805c7daf84cd0032c5edadffc94
Denis Stéphane <stph.denis@gmail.com>
2016-04-11 11:51:52 +0000

    modified:   package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sdenis:static-raw-html',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'It is similar to static-html but get\'s the html and jade files with <template> too.',
   git: 'http://github.com/stphdenis/meteor-static-raw-html.git',
   documentation: 'README.md',
